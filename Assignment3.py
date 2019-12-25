7f41093671c3845de54f48e8bb905d40bcf0b5f7
Saurabh Sarage <57210145+saurabhsarage@users.noreply.github.com>
2019-12-25 19:39:17 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 22:46:40 2019
+
+@author: saurabh
+
+Write a program which accepts a sequence of comma-separated numbers from console and
+generate a list
+"""
+
+n = input("Enter Sequence of comma seperated numbers:- ")
+n1 = (n.split(","))
+print(n1)
